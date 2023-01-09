2d9ab520e78a4ee90b7a1f40dbed95cd8e437678
Artem <artyom.klyuev.01@mail.ru>
2023-01-09 14:25:53 +0300

    Initial Commit

---
+++ Shop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Shop
+//
+//  Created by Artem on 09.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
